073558b9636b42302f6d374ab0b7ab8b3903a8b8
Polina Cherkasova <polina.c@live.com>
2024-05-19 13:27:25 -0700

    Update main.dart

--- website/lib/main.dart
+++ website/lib/main.dart
@@ -9,7 +9,7 @@ import 'shared/framework/scaffold.dart';
 
 const _homeRoute = '/';
 
-final _routes = <String, AppScreen>{
+final _screens = <String, AppScreen>{
   _homeRoute: homeScreen,
   '/explore': exploreScreen,
   '/team': teamScreen,
@@ -30,22 +30,14 @@ class _MainApp extends StatelessWidget {
       initialRoute: _homeRoute,
       // Using this instead of [routes] to turn off animation.
       onGenerateRoute: (settings) {
-        final routeName = settings.name ?? _homeRoute;
+        final route = settings.name ?? _homeRoute;
         return PageRouteBuilder(
-          pageBuilder: (context, __, ___) => _routeEntry(routeName)(context),
+          pageBuilder: (context, __, ___) =>
+              AppScaffold(route, _screens[route] ?? homeScreen),
           transitionsBuilder: (_, __, ___, child) => child,
           settings: settings,
         );
       },
     );
   }
-}
-
-WidgetBuilder _routeEntry(String route) {
-  final screen = _routes[route] ?? homeScreen;
-
-  return (context) => AppScaffold(
-        route,
-        screen,
-      );
 }
