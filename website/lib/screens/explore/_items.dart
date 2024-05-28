3d7006508a3afc8d78c3f0d8d6710dc06dae8176
Polina Cherkasova <polina.c@live.com>
2024-05-27 18:45:35 -0700

    -

--- website/lib/screens/explore/_items.dart
+++ website/lib/screens/explore/_items.dart
@@ -11,15 +11,41 @@ final items = [
     iosLink: _SharedLinks.miraAndGosha,
   ),
   ExploreItem(
+    title: 'Daniel Tiger' 's Feelings',
+    description: 'Play with Daniel Tiger and explore feelings',
+    image: 'images/explore/daniel.webp',
+    iosLink:
+        'https://apps.apple.com/us/app/daniel-tigers-grr-ific-feelings/id899193797',
+    link:
+        'https://play.google.com/store/apps/details?id=org.pbskids.dtgrrificfeelings',
+  ),
+  ExploreItem(
+    title: 'Breathe, Think, Do with Sesame',
+    description: 'Sesame monster is trying to calm down',
+    image: 'images/explore/sesame.webp',
+    iosLink:
+        'https://apps.apple.com/us/app/breathe-think-do-with-sesame/id721853597',
+    link:
+        'https://play.google.com/store/apps/details?id=air.com.sesameworkshop.ResilienceThinkBreathDo',
+  ),
+  ExploreItem(
     title: 'Feeling Arrow',
     description: 'Control people with arrows of feelings',
     image: 'images/explore/feeling_arrow.webp',
     iosLink: 'https://apps.apple.com/us/app/feeling-arrow/id1628597068',
-    link: 'https://play.google.com/store/search?q=feeling+arrow&c=apps',
+    link:
+        'https://play.google.com/store/apps/details?id=com.YsoCorp.Feelingarrow',
+  ),
+  ExploreItem(
+    title: 'Zoo Academy',
+    description: 'Social Emotional Game for K-1 Students',
+    image: 'images/explore/zoo.png',
+    iosLink: 'https://www.centervention.com/zoo-academy/',
+    link: 'https://www.centervention.com/zoo-academy/',
   ),
   ExploreItem(
     title: 'How We Feel',
-    description: 'An emotional wellbeing journal.',
+    description: 'An emotional wellbeing journal',
     image: 'images/explore/how_we_feel.webp',
     iosLink: 'https://apps.apple.com/us/app/how-we-feel/id1562706384',
     link:
